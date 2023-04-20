1d4ba2f054442bc3182b7f146309252e485f88a0
caleb beam <caleb@LAPTOP-IC1KTCC2>
2023-04-20 17:10:10 -0500

    finished reqs

--- gilded-rose.js
+++ gilded-rose.js
@@ -1,3 +1,4 @@
+//dont touch
 export class Item {
   constructor(name, sellIn, quality) {
     this.name = name;
@@ -9,59 +10,117 @@ export class Item {
 export let items = [];
 
 items.push(new Item("+5 Dexterity Vest", 10, 20));
-items.push(new Item("Aged Brie", 2, 0));
+items.push(new Item("Aged Brie", 2, 0)); //increases quality as sellIn goes down
 items.push(new Item("Elixir of the Mongoose", 5, 7));
-items.push(new Item("Sulfuras, Hand of Ragnaros", 0, 80));
-items.push(new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20));
+items.push(new Item("Sulfuras, Hand of Ragnaros", 0, 80)); //no sellIn and quality always 80
+items.push(new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20)); //increase quality as sellIn goes down, after sellIn, quality = 0
 items.push(new Item("Conjured Mana Cake", 3, 6));
+
+// export const updateQuality = () => {
+//   for (let item of items) {
+//     if (
+//       item.name != "Aged Brie" &&
+//       item.name != "Backstage passes to a TAFKAL80ETC concert"
+//     ) {
+//       // normal items that arent at 0 decrease quality
+//       if (item.quality > 0) {
+//         if (
+//           item.name != "Sulfuras, Hand of Ragnaros" &&
+//           !item.name.split(" ").includes("Conjured")
+//         ) {
+//           item.quality = item.quality - 1;
+//         }
+//         if (item.name.split(" ").includes("Conjured")) {
+//           //add conjured item decrease quality
+//           item.quality = item.quality - 2;
+//         }
+//       }
+//     } else {
+//       if (item.quality < 50) {
+//         //increment the Brie if its not max quality
+//         item.quality = item.quality + 1;
+//         if (item.name == "Backstage passes to a TAFKAL80ETC concert") {
+//           if (item.sellIn < 11 && item.quality < 50) {
+//             //increase ticket quality by 1 if less than 11 days and not max quality yet
+//             item.quality = item.quality + 1;
+//           }
+//           if (item.sellIn < 6 && item.quality < 50) {
+//             //increase ticket quality by 3 if less than 6 days and not max quality yet
+//             item.quality = item.quality + 1;
+//           }
+//         }
+//       }
+//     }
+//     if (item.name != "Sulfuras, Hand of Ragnaros") {
+//       item.sellIn = item.sellIn - 1;
+//     }
+//     if (item.sellIn < 0) {
+//       if (item.name != "Aged Brie") {
+//         if (item.name != "Backstage passes to a TAFKAL80ETC concert") {
+//           if (item.quality > 0) {
+//             if (item.name != "Sulfuras, Hand of Ragnaros") {
+//               item.quality = item.quality - 1;
+//             }
+//           }
+//         } else {
+//           item.quality = item.quality - item.quality;
+//         }
+//       } else {
+//         if (item.quality < 50) {
+//           item.quality = item.quality + 1;
+//         }
+//       }
+//     }
+//   }
+// };
 
 export const updateQuality = () => {
   for (let item of items) {
+    //basic decrease checks
     if (
+      item.sellIn >= 0 &&
+      item.quality >= 0 &&
       item.name != "Aged Brie" &&
-      item.name != "Backstage passes to a TAFKAL80ETC concert"
+      item.name != "Sulfuras, Hand of Ragnaros" &&
+      item.name != "Backstage passes to a TAFKAL80ETC concert" &&
+      !item.name.split(" ").includes("Conjured")
     ) {
-      if (item.quality > 0) {
-        if (item.name != "Sulfuras, Hand of Ragnaros") {
-          item.quality = item.quality - 1;
-        }
+      item.quality--;
+      item.sellIn--;
+    }
+    //concert logic
+    if (item.name == "Backstage passes to a TAFKAL80ETC concert") {
+      if (item.sellIn < 11 && item.quality < 50) {
+        item.quality = item.quality + 2;
+        item.sellIn--;
       }
-    } else {
-      if (item.quality < 50) {
-        item.quality = item.quality + 1;
-        if (item.name == "Backstage passes to a TAFKAL80ETC concert") {
-          if (item.sellIn < 11) {
-            if (item.quality < 50) {
-              item.quality = item.quality + 1;
-            }
-          }
-          if (item.sellIn < 6) {
-            if (item.quality < 50) {
-              item.quality = item.quality + 1;
-            }
-          }
-        }
+      if (item.sellIn < 6 && item.quality < 50) {
+        item.quality--;
       }
     }
-    if (item.name != "Sulfuras, Hand of Ragnaros") {
-      item.sellIn = item.sellIn - 1;
+    //conjured logic
+    if (item.name.split(" ").includes("Conjured")) {
+      item.quality -= 2;
+      item.sellIn--;
     }
+    //quality is never more than 50
+    if (item.quality >= 50 && item.name != "Sulfuras, Hand of Ragnaros") {
+      item.quality = 50;
+      item.sellIn--;
+    }
+    //reduce quality twice as fast is sellIn less than zero
     if (item.sellIn < 0) {
-      if (item.name != "Aged Brie") {
-        if (item.name != "Backstage passes to a TAFKAL80ETC concert") {
-          if (item.quality > 0) {
-            if (item.name != "Sulfuras, Hand of Ragnaros") {
-              item.quality = item.quality - 1;
-            }
-          }
-        } else {
-          item.quality = item.quality - item.quality;
-        }
-      } else {
-        if (item.quality < 50) {
-          item.quality = item.quality + 1;
-        }
-      }
+      item.quality -= 2;
+      item.sellIn--;
+    }
+    //quality never negative
+    if (item.quality <= 0) {
+      item.quality = 0;
+    }
+    //Aged Brie Logic
+    if (item.name == "Aged Brie" && item.quality < 50){
+      item.quality++;
+      item.sellIn--;
     }
   }
 };
--- gilded-rose.spec.js
+++ gilded-rose.spec.js
@@ -11,4 +11,76 @@ describe("updateQuality", () => {
     expect(testItem.quality).toBe(2);
     expect(testItem.sellIn).toBe(4);
   });
+
+  it("increases quality of tickets by 2 if less than 11 days to sellIn", () => {
+    const testItem2 = new Item("Backstage passes to a TAFKAL80ETC concert", 9, 20);
+    items.push(testItem2);
+
+    updateQuality();
+
+    expect(testItem2.quality).toBe(22);
+    expect(testItem2.sellIn).toBe(8);
+  });
+
+  it("reduces quality twice as fast if sellIn is less than zero", () => {
+    const testItem3 = new Item("basic", -1, 3);
+    items.push(testItem3);
+
+    updateQuality();
+
+    expect(testItem3.quality).toBe(1);
+    expect(testItem3.sellIn).toBe(-2);
+  });
+
+  it("quality is never less than zero", () => {
+    const testItem4 = new Item("basic", -1, 0);
+    items.push(testItem4);
+
+    updateQuality();
+
+    expect(testItem4.quality).toBe(0);
+    expect(testItem4.sellIn).toBe(-2);
+  });
+
+  it("Aged Brie actually increases in quality the older it gets", () => {
+    const testItem5 = new Item("Aged Brie", 2, 0);
+    items.push(testItem5);
+
+    updateQuality();
+
+    expect(testItem5.quality).toBe(1);
+    expect(testItem5.sellIn).toBe(1);
+  });
+
+  it("The quality of an item is never more than 50.", () => {
+    const testItem5 = new Item("Aged Brie", 10, 50);
+    items.push(testItem5);
+
+    updateQuality();
+
+    expect(testItem5.quality).toBe(50);
+    expect(testItem5.sellIn).toBe(9);
+  });
+
+  it("Sulfuras, Hand of Ragnaros, being a legendary item, never has to be sold nor does it decrease in quality.", () => {
+    const testItem5 = new Item("Sulfuras, Hand of Ragnaros", 0, 80);
+    items.push(testItem5);
+
+    updateQuality();
+
+    expect(testItem5.quality).toBe(80);
+    expect(testItem5.sellIn).toBe(0);
+  });
+
+  it("conjured itmes decrease by 2.", () => {
+    const testItem5 = new Item("Conjured Mana Cake", 3, 6);
+    items.push(testItem5);
+
+    updateQuality();
+
+    expect(testItem5.quality).toBe(4);
+    expect(testItem5.sellIn).toBe(2);
+  });
+
 });
+
